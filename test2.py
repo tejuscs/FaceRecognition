f5db5c9af55fe6bf3bab9557152118624da131c8
Tejus Subrahmanya <tejus@Tejuss-MacBook-Pro.local>
2018-06-29 11:44:09 -0400

    initial commit

---
+++ test2.py
@@ -0,0 +1,31 @@
+import dlib
+import image
+from skimage import io
+import matplotlib.pyplot as plt
+import numpy as np
+
+def detect_faces(image):
+
+    # Create a face detector
+    face_detector = dlib.get_frontal_face_detector()
+
+    # Run detector and get bounding boxes of the faces on image.
+    detected_faces = face_detector(image, 1)
+    face_frames = [(x.left(), x.top(),
+                    x.right(), x.bottom()) for x in detected_faces]
+
+    return face_frames
+
+# Load image
+img_path = '/Users/tejus/Downloads/group-booking.jpg'
+image = io.imread(img_path)
+
+# Detect faces
+detected_faces = detect_faces(image)
+
+# Crop faces and plot
+for n, face_rect in enumerate(detected_faces):
+    face = image.fromarray(image).crop(face_rect)
+    plt.subplot(1, len(detected_faces), n+1)
+    plt.axis('off')
+    plt.imshow(face)
